d082df0249b58d82120d9799983189a16783bb5f
Engr-FahadMughal <engr.fahadmughal@outlook.com>
2021-07-25 20:05:24 +0500

    Added the Navbar and httpClient

--- App-SPA/src/app/app.component.ts
+++ App-SPA/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SportiHub';
+  products: IProduct[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/products/getproducts').subscribe(
+      (response: any) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
