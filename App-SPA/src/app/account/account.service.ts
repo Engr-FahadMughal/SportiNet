dcf068bf2c4abe5f9b5089979512ab83614a1cbe
Engr-FahadMughal <engr.fahadmughal@outlook.com>
2021-08-07 18:52:01 +0500

    Implemented Identity & Validation on Client side

---
+++ App-SPA/src/app/account/account.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { IUser } from './../shared/models/user';
+import { BehaviorSubject, of, ReplaySubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<IUser>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  login(values: any) {
+    return this.httpClient.post(this.baseUrl + 'account/login', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.httpClient.post(this.baseUrl + 'account/register', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string) {
+    return this.httpClient.get(this.baseUrl + 'account/checkemailexists?email=' + email);
+  }
+
+  loadCurrentUser(token: string) {
+    if (token === null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.httpClient.get(this.baseUrl + 'account/getcurrentuser', {headers}).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }  
+}
